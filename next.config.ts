f5bb3aead292665a5c11195e4c11f6d2b96f9455
davidtheosimaremare <davidtheosimaremare@gmail.com>
2026-04-18 08:09:13 +0700

    fix: allow all local assets in localPatterns

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,10 @@ const nextConfig: NextConfig = {
         pathname: '/api/image',
         search: '?url=**',
       },
+      {
+        pathname: '/**',
+        search: '',
+      },
     ],
     remotePatterns: [
       {
